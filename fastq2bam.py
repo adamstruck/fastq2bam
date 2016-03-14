c461348e60696f9cbd9e69b002cfdeae686adcfd
adamstruck <strucka@ohsu.edu>
2016-03-14 13:38:13 -0700

    better error handling

--- fastq2bam.py
+++ fastq2bam.py
@@ -4,7 +4,6 @@ from __future__ import print_function
 
 import argparse
 import dateutil.parser
-import logging
 import os
 import random
 import re
@@ -66,16 +65,29 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 
 
 def execute(cmd):
-    logging.info("RUNNING: %s" % (cmd))
     print("RUNNING...\n", cmd, "\n")
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    stdout, stderr = p.communicate()
-    if stderr is not None:
-        print(stderr)
-    if stdout is not None:
-        print(stdout)
-    return p.returncode
+    process = subprocess.Popen(shlex.split(cmd),
+                               shell=False,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+
+    while True:
+        nextline = process.stdout.readline()
+        if nextline == '' and process.poll() is not None:
+            break
+        sys.stdout.write(nextline)
+        sys.stdout.flush()
+
+    if process.returncode == 0:
+        return process.returncode
+    else:
+        raise RuntimeError(
+            "\n".join(
+                ["",
+                 "[COMMAND]        {0}".format(cmd),
+                 "[STDERR]         {0}".format(process.communicate()[1]),
+                 "[STATUS CODE]    {0}".format(process.returncode)]
+            ))
 
 
 def fastq2bam():
